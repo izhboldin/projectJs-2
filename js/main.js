09936b6e5e4841233a96a1b72a988be8ef70f732
izhboldin <96236560+izhboldin@users.noreply.github.com>
2023-10-06 13:08:45 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
+const truncate = (str, maxlength) => str.length > maxlength ? (str.slice(0, maxlength) + '...') : str;
+
 const zone1 = document.querySelector('.board-column-element-0');
 const zone2 = document.querySelector('.board-column-element-1');
 const zone3 = document.querySelector('.board-column-element-2');
 const zone4 = document.querySelector('.board-column-element-3');
-const taskInZone = document.querySelectorAll('.board-column');
-// console.log(taskInZone);
 
 zone1.ondragover = allowDrop;
 zone2.ondragover = allowDrop;
@@ -14,9 +14,7 @@ function allowDrop(event) {
     event.preventDefault();
 }
 
-taskInZone.forEach((el) => {
-    el.ondragstart = drag;
-})
+const setDrag = () => document.querySelectorAll('.board-column').forEach(el => el.ondragstart = drag);
 
 zone1.ondrop = drop;
 zone2.ondrop = drop;
@@ -24,22 +22,22 @@ zone3.ondrop = drop;
 zone4.ondrop = drop;
 
 function drag(event) {
-    console.log(event.target)
     event.dataTransfer.setData('index', event.target.dataset.index);
     event.dataTransfer.setData('indexG', event.target.closest('.board-column-element').dataset.index);
-
 }
 
-function drop(event){
+function drop(event) {
     let itemIndex = event.dataTransfer.getData('index');
     let itemsIndex = event.dataTransfer.getData('IndexG');
 
-    // event.target.append()
-    if(event.target.className.split(' ')[1] === 'board-column-element'){
-       tasks[event.target.dataset.index].push(tasks[itemsIndex][itemIndex]);
-       tasks[itemsIndex].splice(itemIndex, 1);
-       saveTasks();
-       showTask(tasks);
-       location.reload();
+    if(itemIndex == '' || itemsIndex == ''){
+        return
+    }
+
+    if (event.target.className.split(' ')[1] === 'board-column-element') {
+        tasks[event.target.dataset.index].push(tasks[itemsIndex][itemIndex]);
+        tasks[itemsIndex].splice(itemIndex, 1);
+        saveTasks();
+        showTask(tasks);
     }
 }
